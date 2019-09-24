6461e5a89cb92e03f114b87d3b66b5af1a534c2f
jacek_sanko <jacek@housetipster.com>
2019-09-24 08:40:10 +0200

    Initalized redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './css/index.scss';
 import App from './components/App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 
